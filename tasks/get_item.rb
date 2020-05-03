31c6e961f5b7569777770f0bcada3f2527a0e959
Charlie Sharpsteen <source@sharpsteen.net>
2020-05-03 14:24:15 -0700

    Implement selection of values from 1password data
    This commit implements a `select` parameter for the `op_data::get_item`
    task. This parameter can be used to drill into 1password data using
    dotted paths to retrieve nested values.
    
    E.g. "select: details.password" can be used to pull the "password" field
    out of the "details" field of a 1password item.

--- tasks/get_item.rb
+++ tasks/get_item.rb
@@ -7,10 +7,14 @@ require 'open3'
 require_relative '../../ruby_task_helper/files/task_helper.rb'
 
 class OpDataGetItem < TaskHelper
-  def task(account:, id:, extract: nil, vault: nil, **opts)
+  def task(account:, id:, select: nil, vault: nil, **opts)
     connect_1password(account)
 
-    data = get_item(id, vault)
+    data = if select.nil?
+             get_item(id, vault)
+           else
+             select_value(get_item(id, vault), select)
+           end
 
     { value: data }
   end
@@ -60,6 +64,49 @@ class OpDataGetItem < TaskHelper
                                     { code: status.exitstatus },
                                   'op_data::get_item/get-item-failed',
                                   debug: stderr)
+    end
+  end
+
+  # Select a value from a nested data structure
+  #
+  # This function takes a data structure and a string containing a dotted path,
+  # such as "details.password", and fetches the value at the path.
+  #
+  # @param data [Hash, Array] A nested structure of Hashes and Arrays.
+  # @param path [String] A dot-delimited path that identifies a value inside
+  #   a nested structure of Hashes and Arrays.
+  #
+  # @return [Object] The selected value.
+  def select_value(data, path)
+    value = data.dig(*path_to_dig(path))
+
+    if value.nil?
+      # TODO: Raise error.
+    else
+      value
+    end
+  end
+
+  # Convert a dotted path to arguments for dig
+  #
+  # This function takes a string containing a dotted path, such as
+  # "foo.bar" and produces an argument vector for Ruby's `dig` function.
+  #
+  # @param path [String] A dot-delimited path that identifies a value inside
+  #   a nested structure of Hashes and Arrays.
+  #
+  # @return [Array<String, Integer>] An argument list of string indices for
+  #   Hashes and integer indices for Arrays that can be passed to the
+  #   Ruby `dig` function.
+  def path_to_dig(path)
+    path.split('.').map do |e|
+      case e
+      when /\d+/
+        # Strings of integers are interpreted as array indices
+        Integer(e)
+      else
+        e
+      end
     end
   end
 end
