a76534c9787510d08d612de0d77c055f4b25fe5e
Charlie Sharpsteen <source@sharpsteen.net>
2020-05-04 10:10:48 -0700

    Add tests and error handling
    This commit updates the `op_data::get_item` task with better
    documentation and error handling and adds RSpec tests that
    exercise task behavior.

--- tasks/get_item.rb
+++ tasks/get_item.rb
@@ -7,7 +7,16 @@ require 'open3'
 require_relative '../../ruby_task_helper/files/task_helper.rb'
 
 class OpDataGetItem < TaskHelper
+  # @return [String] path to the 1password CLI binary.
+  # @return [nil] when no 1password CLI is present.
+  #
+  # @see #connect_1password
+  attr_reader :op_cli
+
   def task(account:, id:, select: nil, vault: nil, **opts)
+    # Store arguments for use in error messages.
+    @account, @id, @select, @vault = account, id, select, vault
+
     connect_1password(account)
 
     data = if select.nil?
@@ -19,17 +28,37 @@ class OpDataGetItem < TaskHelper
     { value: data }
   end
 
+  # Find the 1password CLI and check configuration
+  #
+  # This method locates the 1password CLI on the `$PATH` and ensures
+  # that account credentials have been set in the environment.
+  #
+  # @param account [String] the name or alias of the 1password account to
+  #   fetch data from.
+  #
+  # @raise [TaskHelper::Error] if the `op` CLI cannot be found or credentials
+  #   for the `account` are not set in the environment.
+  #
+  # @return [void]
   def connect_1password(account)
+    # TODO: Fail on Windows. For now.
     @op_cli, have_op = Open3.capture2('/bin/sh', '-c', 'command -v op')
 
     if have_op.success?
       @op_cli.chomp!
     else
-      # TODO: Return an error because the op CLI is missing
+      raise TaskHelper::Error.new('Could not find the `op` command. Check the package is installed and the $PATH is configured: https://support.1password.com/command-line-getting-started/',
+                                  'op_data/cli-missing',
+                                  debug: ENV['PATH'])
     end
 
     credential = resolve_credential(account)
-    # TODO: Return an error if ENV[credential] is unset
+    unless ENV.key?(credential)
+      raise TaskHelper::Error.new('No credentials in environment variable %{env_var}. Did you run `op signin %{account}` and export the result?' %
+                                    {account: account,
+                                     env_var: credential},
+                                  'op_data/credential-missing')
+    end
   end
 
   # Convert an account name to the name of a 1password credential
@@ -51,18 +80,39 @@ class OpDataGetItem < TaskHelper
     "OP_SESSION_#{name}"
   end
 
+  # Retrieve item data from 1password
+  #
+  # This method uses the `op` CLI to retrieve an item from
+  # 1password vaults.
+  #
+  # @param id [String] the name or UUID of the item to retrieve.
+  # @param vault [String, nil] the name or UUID of the vault to
+  #   look for the item in (optional).
+  #
+  # @raise [TaskHelper::Error] if execution of `op` returns an
+  #   error.
+  #
+  # @return [Hash] the result of parsing the JSON representation
+  #   of the item.
   def get_item(id, vault = nil)
-    cmdline = [@op_cli, 'get', 'item', id]
+    cmdline = [self.op_cli, 'get', 'item', id]
     cmdline.concat(['--vault', vault]) unless vault.nil?
 
+    # TODO: `op get item` makes network calls. Should we
+    #   have a defensive timeout here?
     stdout, stderr, status = Open3.capture3(*cmdline)
 
     if status.success?
       JSON.parse(stdout)
     else
-      raise TaskHelper::Error.new("'op get item' exited with error code: %{code}" %
-                                    { code: status.exitstatus },
-                                  'op_data::get_item/get-item-failed',
+      # The first "[LOG]" line often has details of what went wrong.
+      err_msg = stderr.lines.find {|l| l.start_with?('[LOG]') }
+      err_msg.chomp! unless err_msg.nil?
+
+      raise TaskHelper::Error.new('`op get item` exited with error code %{code}: %{msg}' %
+                                    {code: status.exitstatus,
+                                     msg: err_msg},
+                                  'op_data/get-item-failed',
                                   debug: stderr)
     end
   end
@@ -76,15 +126,32 @@ class OpDataGetItem < TaskHelper
   # @param path [String] A dot-delimited path that identifies a value inside
   #   a nested structure of Hashes and Arrays.
   #
+  # @raise [TaskHelper::Error] if an error occurs while selecting data.
+  #
   # @return [Object] The selected value.
   def select_value(data, path)
     value = data.dig(*path_to_dig(path))
 
     if value.nil?
-      # TODO: Raise error.
+      raise KeyError.new('no value found at path')
     else
       value
     end
+  rescue StandardError => e
+    errstring = if @vault.nil?
+                  'Error while selecting %{path} from %{id} in %{account}: %{msg} (%{errclass})'
+                else
+                  'Error while selecting %{path} from %{id} in %{vault} in %{account}: %{msg} (%{errclass})'
+                end
+
+    raise TaskHelper::Error.new(errstring %
+                                  {path: path,
+                                   id: @id,
+                                   account: @account,
+                                   vault: @vault,
+                                   msg: e.message,
+                                   errclass: e.class.name},
+                                'op_data/select-failed')
   end
 
   # Convert a dotted path to arguments for dig
