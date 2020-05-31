044287aae7338e7596af5d2540fec6e95c6c3fee
Charlie Sharpsteen <source@sharpsteen.net>
2020-05-31 13:12:38 -0700

    Update to PDK 1.18.0
    This commit updates to PDK 1.18.0 and adds a .sync.yml file with
    module-specific customizations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 
 RSpec.configure do |c|
   c.mock_with :rspec
-  c.raise_errors_for_deprecations!
 end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
@@ -43,6 +42,7 @@ RSpec.configure do |c|
     # set to strictest setting for testing
     # by default Puppet runs at warning level
     Puppet.settings[:strict] = :warning
+    Puppet.settings[:strict_variables] = true
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
